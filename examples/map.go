417b7e788adea4dd7201db17f11acfdb6afe3ceb
ucwong <ucwong@126.com>
2022-10-20 13:14:01 +0800

    fix

--- examples/map.go
+++ examples/map.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/imkira/go-ttlmap"
+	"github.com/ucwong/go-ttlmap"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/ucwong/go-ttlmap
 
 go 1.19
-
-require github.com/imkira/go-ttlmap v2.0.0+incompatible
